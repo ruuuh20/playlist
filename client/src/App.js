c0796bba01188bf3ca9ee6a90184883f0ccc6e60
ruuuh20 <pkim2320@gmail.com>
2019-02-24 17:27:35 -0500

    rename app and add track playlist models

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Playlist app</h1>
+      <h1>PlayMixList app</h1>
       <h2><a href="/artists">Artists</a></h2>
         <Login currentUser={this.state.currentUser} />
         <Route path="/callback" component={this.handleCallback} />
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_204732) do
+ActiveRecord::Schema.define(version: 2019_02_24_221139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,27 @@ ActiveRecord::Schema.define(version: 2019_02_09_204732) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "playlist_tracks", force: :cascade do |t|
+    t.integer "playlist_id"
+    t.integer "track_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.string "create"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
